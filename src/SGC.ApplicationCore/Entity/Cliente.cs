fee06bc80f9ab86004accf5daf1dff79d9b00897
Mariano Vunge <HCKMariano@gmail.com>
2019-02-08 00:43:36 +0000

    Cria a classe ClienteContext e adiciona Migration

---
+++ src/SGC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace SGC.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public string ClienteNome { get; set; }
+        public string NIP { get; set; }
+    }
+}
